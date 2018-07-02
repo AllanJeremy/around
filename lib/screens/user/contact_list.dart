a41d3fb33db2e9f0045006727bbe154eadf8b4e5
AllanJeremy <aj.dev254@gmail.com>
2018-07-02 18:50:45 +0300

    Contact list base

--- lib/screens/user/contact_list.dart
+++ lib/screens/user/contact_list.dart
@@ -1,1 +1,19 @@
-// import 'dart:async';
+import 'package:flutter/material.dart';
+
+class ContactList extends StatefulWidget{
+    @override
+    createState() => _ContactListState();
+}
+
+class _ContactListState extends State<ContactList>{
+    
+    @override 
+    initState(){
+        super.initState();
+        
+    }
+    @override
+    Widget build(BuildContext context){
+        return Container();
+    }
+}
