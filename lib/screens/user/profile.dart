03e41a9f9be5045eecad4fa9d530c5334ec6b15c
AllanJeremy <aj.dev254@gmail.com>
2018-07-03 22:09:06 +0300

    Renamed chats to chat_list and added base profile UI

--- lib/screens/user/profile.dart
+++ lib/screens/user/profile.dart
@@ -1,1 +1,34 @@
-// User profile ~ takes in a user_id
+import 'package:flutter/material.dart';
+
+class UserProfile extends StatelessWidget{
+
+    @override
+    Widget build(BuildContext context){
+        return Container(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+                children: <Widget>[
+                    Container(
+                        padding: EdgeInsets.all(32.0),
+                        color: Colors.grey,
+                        child: Center(
+                            child: CircleAvatar(
+                                backgroundColor: Colors.grey,
+                                minRadius: 96.0,
+                                maxRadius: 256.0,
+                                child:FloatingActionButton(
+                                    backgroundColor: Colors.blue,//TODO : Change to app theme color
+                                    onPressed: ()=>print('Edit picture'),//TODO: Callback for edit picture
+                                    child: Icon(Icons.camera_alt,
+                                        size:48.0
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ),
+
+                ],
+            ),
+        );
+    }
+}
